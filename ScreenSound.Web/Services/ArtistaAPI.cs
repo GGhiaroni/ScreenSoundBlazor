2c2894d75b958d3fe7282e631775c8cf29582684
GGhiaroni <gghiaronitiziano@gmail.com>
2024-09-26 12:11:09 -0300

    criacao rota artistas e cadastrar artistas

---
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -0,0 +1,22 @@
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class ArtistaAPI
+{
+    private readonly HttpClient _httpClient;
+    public ArtistaAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+    public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
+    public async Task AddArtistaAsync(ArtistaRequest artista)
+    {
+        await _httpClient.PostAsJsonAsync("artistas", artista);
+    }
+}
